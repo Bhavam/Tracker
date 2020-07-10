6732307e767667674a4b235fe3a0d051ed96d1b4
Bhavam Vidyathi <bhavam.vidyarthi@gmail.com>
2020-07-10 21:46:51 +0530

    Modification Log #4

--- tracktor.py
+++ tracktor.py
@@ -5,11 +5,13 @@ import motmetrics as mm
 import torch
 import torchvision
 import csv
+import matplotlib.pyplot as plt
 
 #path declarations hardcoded for the moment
-path_image=r'C:/Users/HP.HP-PC/tracking_wo_bnw/data/MOT17Det/train/MOT17-02/img1'
+path_image=r'C:/Users/HP.HP-PC/tracking_wo_bnw/data/MOT17Det/train/MOT17-02/img3'
 out_path_image=r'C:/Users/HP.HP-PC/tracking_wo_bnw/data/MOT17Det/train/MOT17-02/img2'
 path_box=r'C:/Users/HP.HP-PC/tracking_wo_bnw/data/MOT17Det/train/MOT17-02/gt/gt.txt'
+img_path_ex = r'C:/Users/HP.HP-PC/tracking_wo_bnw/data/MOT17Det/train/MOT17-02/img4/000001.jpg'
 
 #Class to save box details
 class box:
@@ -41,27 +43,43 @@ class tracktor:
         self.training_data = []
         self.boxes = []
         self.score = []
+        self.threshold = 0.5
         self.object_detector = object_detector
         self.run()
          
      def run(self):
          #self.read_detections()
-         self.predict_boxes()
+         self.plot_boxes()
          #self.write_boxes()
+         #self.predict_one(img_path_ex)
          
-     def predict_boxes(self):
-        #figure out output of boxes, scores to fit to custom bounding box class
-        for img_path in os.listdir(path_image): 
+     def predict_boxes(self,img_path):
+        #figure out output of boxes, scores to fit to custom bounding box class 
             total_img_path = os.path.join(path_image , img_path)
             image = cv2.imread(total_img_path)
-            transform = torch.Compose([torch.ToTensor()]) 
-            #image = transform(image)
-            self.object_detector.load_image(image)
-            b,s = self.object_detector.detect(image)
-            self.boxes.append(b)
-            self.score.append(s)
-     def write_boxes(self):
+            transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()]) 
+            image = transform(image)
+            pred = self.object_detector([image]) 
+            #pred_class = [self.category[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
+            boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] 
+            self.score = list(pred[0]['scores'].detach().numpy())
+            pred_t = [self.score.index(x) for x in self.score if x > self.threshold][-1]
+            self.boxes = boxes[:pred_t+1]
+            #pred_class = pred_class[:pred_t+1]
+            return self.boxes,self.score
 
+     def plot_boxes(self):
+         for img_path in os.listdir(path_image):
+             bb,s = self.predict_boxes(img_path)
+             img = cv2.imread(img_path)
+             for i in range(len(bb)):
+                 cv2.rectangle(img, bb[i][0], bb[i][1],(255 , 0 , 0),2)
+             cv2.imshow('x',img)
+             cv2.waitKey(0)
+             cv2.destroyAllWindows()
+
+         
+     def write_boxes(self):
         #predicting bounding boxes for each video frame  
         self.frame_count = 0
         self.images = []
@@ -91,15 +109,20 @@ class tracktor:
             out = os.path.join(out_path_image,'predicted_'+img_path)
             cv2.imwrite(out,predicted_image)
    
-     def read_detections(self):
+     def read_detections1(self):
         #reading bounding box information
         with open(path_box) as bb_list:
                 data = csv.reader(bb_list , delimiter = ',')
                 for row in data:
                    #make this a 2D dictionary to decrease loop complexity
                    self.training_data.append(box(float(row[0]),float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5]),float(row[6]),float(row[7]),float(row[8])))
-        self.box_count = len(self.training_data) 
+        self.box_count = len(self.training_data)   
 
-#if __name__ == '__main__':
-    #tr = tracktor()
-    #tr.run()
+     def predict_one(self,img_path):
+             bb,s = self.predict_boxes(img_path)
+             img = cv2.imread(img_path)
+             for i in range(len(bb)):
+                 cv2.rectangle(img, bb[i][0], bb[i][1],(255 , 0 , 0),2)
+             cv2.imshow('x',img)
+             cv2.waitKey(0)
+             cv2.destroyAllWindows()
