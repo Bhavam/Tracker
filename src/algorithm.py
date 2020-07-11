0b04e14a5507b346daf5a59c9af8fdfdadd8dc15
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-07-11 23:05:27 +0530

    added algorithm.py

---
+++ src/algorithm.py
@@ -0,0 +1,172 @@
+import cv2
+import os
+import numpy as np
+import motmetrics as mm
+import torch
+import torchvision
+import csv
+import matplotlib.pyplot as plt
+
+#path declarations hardcoded for the moment
+path_image=r'C:/Users/HP.HP-PC/tracking_wo_bnw/data/MOT17Det/train/MOT17-02/img1'
+out_path_image=r'C:/Users/HP.HP-PC/tracking_wo_bnw/data/MOT17Det/train/MOT17-02/img2'
+path_box=r'C:/Users/HP.HP-PC/tracking_wo_bnw/data/MOT17Det/train/MOT17-02/gt/gt.txt'
+
+class Track :
+    def __init__(self):
+        self.track = []
+    def addBoxToTrack(self,box):
+        self.track.append(box)
+
+#Class to save box details
+class box:
+    def __init__(self,frame_id,box_id,left,top,width,depth,x,y,z):
+            self.frame_id = frame_id
+            self.box_id = box_id
+            self.bb_left = left
+            self.bb_top = top
+            self.bb_width = width
+            self.bb_depth = depth
+            self.x = x
+            self.y = y
+            self.z = z
+ 
+    def create_frame_list(self,boxes):
+        frame_map = {}
+        for bx in boxes:
+                if bx.frame_id in frame_map:
+                    frame_map[bx.frame_id].append(bx.box_id)
+                else:
+                    frame_map[bx.frame_id] = [bx.box_id]
+        
+        return frame_map      
+
+#main tracktor class
+class tracktor:
+     def __init__(self,object_detector): #,reid_network
+         #self.object_detector = object_detector
+         #self.reid_network = reid_network
+         #figure out parameters to be minimally extracted from config file
+        self.category = ['person']
+        self.training_data = []
+        self.boxes = []
+        self.score = []
+        self.threshold = 0.5
+        self.object_detector = object_detector
+        self.run()
+        self.imge = None
+         
+     def run(self):
+         #self.read_detections()
+         self.plot_boxes()
+         #self.write_boxes()
+        #  self.predict_one(img_path_ex)
+         
+     def predict_boxes(self,img_path):
+        #figure out output of boxes, scores to fit to custom bounding box class 
+            image = cv2.imread(img_path)
+            transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()]) 
+            image = transform(image)
+            pred = self.object_detector([image]) 
+            #pred_class = [self.category[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
+            boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] 
+            self.score = list(pred[0]['scores'].detach().numpy())
+            pred_t = [self.score.index(x) for x in self.score if x > self.threshold][-1]
+            self.boxes = boxes[:pred_t+1]
+            #pred_class = pred_class[:pred_t+1]
+            return self.boxes,self.score
+
+     def plot_boxes(self):
+         setOfActiveTracks = []
+         setOfTracks = []
+         for img_path in os.listdir(path_image):
+             S=[]   
+             B=[]
+             for activeTrack in setOfActiveTracks:
+                 bprev = activeTrack[-1]
+
+                 total_img_path = os.path.join(path_image , img_path)
+                 bb,s = self.predict_boxes(total_img_path)
+
+                 img = cv2.imread(img_path)
+                 sigma =0.5
+                 if s < 0.5 :
+                     setOfActiveTracks.remove(activeTrack)
+                     setOfTracks.append(activeTrack)
+                 else:
+                     B.append(bb)
+                     S.append(s)
+                 # NMS For B <- NMS(B,S,lambda)
+                 # ignoring lines 14 to 17
+                 for tk,bk in zip(setOfActiveTracks,B):
+                     tk.addBoxToTrack(bk)
+                 Dt,s = self.predict_boxes(total_img_path)
+                 for i in range(len(Dt)):
+                     tk = Track()
+                     tk.addBoxToTrack(Dt[i])
+                     setOfActiveTracks.append(tk)
+                 
+             
+                #  cv2.rectangle(img, bb[i][0], bb[i][1],(255 , 0 , 0),2)
+            #  cv2.imwrite(out_path_image+"pred00"+str(i)+".jpg",img)
+             cv2.waitKey(0)
+             cv2.destroyAllWindows()
+         setOfTracks.append(setOfActiveTracks)
+     def algo(self):
+         for img_path in os.listdir(path_image):
+             total_img_path = os.path.join(path_image , img_path)
+             bb,s = self.predict_boxes(total_img_path)
+             img = cv2.imread(img_path)
+             for i in range(len(bb)):
+                 cv2.rectangle(img, bb[i][0], bb[i][1],(255 , 0 , 0),2)
+             cv2.imwrite(out_path_image+"pred00"+str(i)+".jpg",img)
+             cv2.waitKey(0)
+             cv2.destroyAllWindows()
+
+         
+     def write_boxes(self):
+        #predicting bounding boxes for each video frame  
+        self.frame_count = 0
+        self.images = []
+        for img_path in os.listdir(path_image):
+            total_img_path = os.path.join(path_image , img_path)
+            image = cv2.imread(total_img_path)
+            self.training_data.append(image)
+            idx = self.frame_count
+            predicted_image = image
+
+            for i in range(self.box_count):
+                   if self.training_data[i].frame_id == idx:
+                           cv2.rectangle(predicted_image,
+                                (int(self.training_data[i].bb_left),int(self.training_data[i].bb_top)),
+                                (int(self.training_data[i].bb_left + self.training_data[i].bb_width),int(self.training_data[i].bb_top + self.training_data[i].bb_depth)),
+                                (255,0,0) ,2)
+
+            self.frame_count = self.frame_count + 1
+
+            #Uncomment to see predicted video frames one by one 
+            """ cv2.imshow('x',predicted_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()   """  
+            #writing output images to output folder
+            os.chdir(out_path_image)
+            out = os.path.join(out_path_image,'predicted_'+img_path)
+            cv2.imwrite(out,predicted_image)
+   
+     def read_detections(self):
+        #reading bounding box information
+        with open(path_box) as bb_list:
+                data = csv.reader(bb_list , delimiter = ',')
+                for row in data:
+                   #make this a 2D dictionary to decrease loop complexity
+                   self.training_data.append(box(float(row[0]),float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5]),float(row[6]),float(row[7]),float(row[8])))
+        self.box_count = len(self.training_data)   
+
+     def predict_one(self,img_path):
+             bb,s = self.predict_boxes(img_path)
+             img = cv2.imread(img_path)
+             for i in range(len(bb)):
+                 cv2.rectangle(img, bb[i][0], bb[i][1],(255 , 0 , 0),2)
+             cv2.imwrite(out_path_image,img)
+             cv2.waitKey(0)
+             cv2.destroyAllWindows()
